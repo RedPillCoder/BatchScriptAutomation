40ac6e08b71d3f806598ffef1003ed9da8fba593
RedPillCoder <Tappilina@tutanota.com>
2025-02-02 01:15:40 +0800

    Update Organize Downloads by File Type.bat (#1)
    Signed-off-by: RedPillCoder

--- Types of bat files/File Management/Organize Downloads by File Type.bat
+++ Types of bat files/File Management/Organize Downloads by File Type.bat
@@ -17,10 +17,16 @@ cd /d "%source%"
 :: Organize files by type
 for %%f in (*.*) do (
     set "ext=%%~xf"
-    :: Create the subfolder for the file type if it doesn't exist
-    mkdir "!ext!" 2>nul
-    :: Move the file to the appropriate subfolder
-    move "%%f" "!ext!\" >nul
+    :: Remove the leading dot from the file extension
+    set "ext=!ext:~1!"
+    
+    :: Check if the extension is not empty
+    if not "!ext!"=="" (
+        :: Create the subfolder for the file type if it doesn't exist
+        mkdir "!ext!" 2>nul
+        :: Move the file to the appropriate subfolder
+        move "%%f" "!ext!\" >nul
+    )
 )
 
 echo Downloads organized by file type.
